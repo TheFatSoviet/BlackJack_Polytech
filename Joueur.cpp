22e27f5ca0913077ffe17df3d34eb0149a034d35
Mathis <pascal.mathis222@gmail.com>
2024-03-19 14:30:38 +0100

    Class joueur

---
+++ Joueur.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Joueur
+{
+public:
+    char nom[20];
+    char type_joueur[20];
+    int score_in_game;
+    int nb_partie_joue;
+    int jeton_possede;
+    int jeton_mise;
+
+
+    void creation_joueur(char *nom[20])
+    {
+      
+    }
+};
---
+++ main_mathis.cpp
@@ -0,0 +1,12 @@
+#include "Joueur.cpp"
+
+
+int main()
+{
+    int nb_joueur;
+
+    printf("Saisir le nombre de joueur");
+    scanf("%d",&nb_joueur);
+
+    return 0;
+}
