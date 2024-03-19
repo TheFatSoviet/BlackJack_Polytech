2917f0065946d1e8d82910156e966aba0c257677
TheFatSoviet <ratchet010@gmail.com>
2024-03-19 15:06:43 +0100

    test

--- Joueur.cpp
+++ Joueur.cpp
@@ -14,6 +14,15 @@ public:
     int jeton_possede;
     int jeton_mise;
 
+    Joueur() {
+    nom[0] = '\0'; // Initialise le nom avec une chaîne vide
+    type_joueur[0] = '\0'; // Initialise le type de joueur avec une chaîne vide
+    score_in_game = 0;
+    nb_partie_joue = 0;
+    jeton_possede = 0;
+    jeton_mise = 0;
+  }//
+
 
     void creation_joueur(const char* nouveau_nom)
     {
