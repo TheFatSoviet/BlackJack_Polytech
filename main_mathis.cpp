97c029a14c091fe970bf25cc01283e180c7c2f39
Mathis <pascal.mathis222@gmail.com>
2024-03-19 16:15:52 +0100

    test bool

--- main_mathis.cpp
+++ main_mathis.cpp
@@ -1,10 +1,38 @@
+#include "tir16.cpp"
+#include <iostream>
+#include <stdlib.h>
+#include <random>
 
-#include "rand.cpp"
+using namespace std;
 
 
 int main()
 {
-  
+    vector<Joueur> joueurs(3);
+
+    bool ouii;
+    char c1[20]= "tir16";
+
+    for(int i=0;i<3;i++)
+    {
+      strcpy(joueurs[i].type_joueur,c1);
+    }
+
+    for(int i=0;i<3;i++)
+    {
+
+      if(strcmp(joueurs[i].type_joueur,"tir16")==0)
+      {
+        tir16 test;
+
+        ouii=test.pioche_tir16(20);
+
+        cout<<ouii;
+      }
+    }
+
+
+
 
     return 0;
 }
