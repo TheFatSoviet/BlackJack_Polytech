59abc0c170b1fce29004d7ce86d1e9cd63cb6c80
Adam <adam_musharraf@yahoo.com>
2024-03-19 15:31:23 +0100

    Modif_courpier cpp

--- Courpier.cpp
+++ Courpier.cpp
@@ -3,12 +3,18 @@
 
 class courpier{
 public:
-    bool pioche(int score_in_game){
-        if (score_in_game > 17){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
+    bool pioche_courpier(int score_in_game);
 };
+
+
+
+
+
+bool pioche_courpier(int score_in_game){
+    if (score_in_game > 17){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
