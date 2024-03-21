9c16cdcb10909f74c473643aa584578a324da21b
Adam <adam_musharraf@yahoo.com>
2024-03-21 20:36:37 +0100

    class humain modifier et fonctionne correctement

--- Humain.cpp
+++ Humain.cpp
@@ -6,19 +6,20 @@ using namespace std;
 class humain{
 public:
     // Méthode pour demander à l'utilisateur s'il souhaite tirer une carte.
-    bool pioche_humain() {
+    bool pioche_humain(int score) {
         char oui_non; // Variable pour stocker la réponse de l'utilisateur.
-        while (true) { // Boucle jusqu'à ce que l'utilisateur entre une réponse valide.
+            if (score >= 21){
+              return false;
+            }
             cout << "Voulez-vous tirer une carte ? (o/n)" << endl;
             cin >> oui_non;
+            cout << "score in game " << score << endl;
             if (oui_non == 'o') {
                 return true; // Retourne vrai si l'utilisateur veut tirer une carte.
             } else if (oui_non == 'n') {
                 return false; // Retourne faux si l'utilisateur ne veut pas tirer de carte.
             } else {
                 cout << "Entrée invalide. Veuillez répondre par 'o' ou 'n'." << endl;
-                // Aucun changement de temp ou autre variable nécessaire, la boucle continue.
             }
         }
-    }
 };
